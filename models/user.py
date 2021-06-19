550a1b9482c72874d2742ba98681fb68682200ae
Marcos Fontana <fontana.marcos@gmail.com>
2021-06-19 20:07:14 -0300

    Project structure change

--- models/user.py
+++ models/user.py
@@ -1,5 +1,4 @@
 import sqlite3
-from flask_restful import Resource, Api, reqparse
 
 class User:
     def __init__(self, _id, username, password): # id is a reserved keyword
@@ -38,32 +37,3 @@ class User:
             user = None
         connection.close()
         return user
-
-class UserRegister(Resource):
-    parser = reqparse.RequestParser()
-    parser.add_argument('username',
-        type=str,
-        required=True,
-        help="This field cannot bet left blank!"
-    )
-    parser.add_argument('password',
-        type=str,
-        required=True,
-        help="This field cannot bet left blank!"
-    )         
-    def post(self):
-        data = UserRegister.parser.parse_args()
-        if User.find_by_username(data['username']):
-            return {"message": "User already exists."}, 400
-
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO users VALUES (NULL, ?, ?)"
-        cursor.execute(query, (data['username'], data['password']))
-
-        connection.commit()
-        connection.close()
-
-        return {"message": "User created successfully."}, 201
-            
---
+++ resources/user.py
@@ -0,0 +1,31 @@
+import sqlite3
+from flask_restful import Resource, Api, reqparse
+
+class UserRegister(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('username',
+        type=str,
+        required=True,
+        help="This field cannot bet left blank!"
+    )
+    parser.add_argument('password',
+        type=str,
+        required=True,
+        help="This field cannot bet left blank!"
+    )         
+    def post(self):
+        data = UserRegister.parser.parse_args()
+        if User.find_by_username(data['username']):
+            return {"message": "User already exists."}, 400
+
+        connection = sqlite3.connect('data.db')
+        cursor = connection.cursor()
+
+        query = "INSERT INTO users VALUES (NULL, ?, ?)"
+        cursor.execute(query, (data['username'], data['password']))
+
+        connection.commit()
+        connection.close()
+
+        return {"message": "User created successfully."}, 201
+            
